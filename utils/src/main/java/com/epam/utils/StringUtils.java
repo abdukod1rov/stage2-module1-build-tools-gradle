9dd978c66dee12d61db151895496169f8bed7d6d
sabdukodirov <abdukodirovsamandar3@gmail.com>
2024-05-29 16:43:36 +0500

    'first-commit'

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,13 @@
 package com.epam.utils;
+import org.apache.commons.lang3.math.NumberUtils;
+
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          //here magic will happen
+        if (NumberUtils.isCreatable(str)) {
+            return NumberUtils.createNumber(str).doubleValue() > 0;
+        }
+        return false;
     }
 }
